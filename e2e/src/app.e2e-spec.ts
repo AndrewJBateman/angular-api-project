af468254d015994b4f05f1719f4bdf6f6866f1b3
AndrewJBateman <gomezbateman@yahoo.com>
2023-07-20 06:25:11 +0200

    fi: corrected app name in build scripts

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to Angular7-API-project!');
+    expect(page.getParagraphText()).toEqual('Welcome to angular-api-project!');
   });
 });
